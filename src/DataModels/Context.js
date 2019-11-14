aed46a8c59c8c92ca9f63d05aa74186a853c11bc
Ryan Krol <ryankr@hotmail.co.uk>
2019-11-14 23:31:10 +0000

    Add Context class

---
+++ src/DataModels/Context.js
@@ -0,0 +1,19 @@
+// Holds certain elements of state about the program
+//  * validCsvFields - these will be used to validate the hooks
+//    that the user is trying to pull from config
+
+export class Context {
+
+  constructor(jsonString) {
+    this._validateParams(jsonString)
+
+    this.validCsvFields = jsonString.CSVFields
+  }
+
+  _validateParams(jsonString) {
+    if (!jsonString.CSVFields) {
+      throw new Error('Could not parse CSVFields from config')
+    }
+  }
+
+}
