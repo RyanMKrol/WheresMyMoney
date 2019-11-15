e986a4fad1e3c27478890c253704bd6c3cdae87d
Ryan Krol <ryankr@hotmail.co.uk>
2019-11-15 00:01:06 +0000

    Clean up app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,33 @@
-const csv = require('csv-parser')
-const fs = require('fs')
-const results = [];
+import { Group } from './DataModels/Group'
+import { Context } from './DataModels/Context'
+import csv from 'csv-parser'
+import fs from 'fs'
 
-fs.createReadStream('fileToParse.csv')
-  .pipe(csv())
-  .on('data', (data) => results.push(data))
-  .on('end', () => {
-    console.log(results);
-  });
+const CSV_LOCATION = 'fileToParse.csv'
+
+const csvEntries = readCsvFile(CSV_LOCATION)
+
+readConfig().then((config) => {
+  const context = new Context(config)
+  const myGroups = config.Groups.map((group) => new Group(group, context))
+})
+
+function readCsvFile(fileLocation) {
+  let csvEntries = []
+  fs.createReadStream(fileLocation)
+    .pipe(csv())
+    .on('data', (data) => csvEntries.push(data))
+
+  return csvEntries
+}
+function readConfig() {
+  return new Promise((resolve, reject) => {
+    fs.readFile('config.json', 'utf8', (err, rawJson) => {
+      if (err) {
+          reject(`Config read failed: ${err}`)
+      }
+
+      resolve(JSON.parse(rawJson))
+    })
+  })
+}
