d8aa89c5b7eae4b1ace4e62306ce46127f50eaba
Ryan Krol <ryankr@hotmail.co.uk>
2019-11-15 20:00:46 +0000

    Add income report data to report builder

--- src/Lib/ReportBuilderLib.js
+++ src/Lib/ReportBuilderLib.js
@@ -1,38 +1,62 @@
 import {
   extractDate,
-  extractOutgoings,
+  extractPaidOut,
+  extractPaidIn,
   extractDescription,
 } from './CSVEntryLib'
 
 import { DEFAULT_GROUP } from './../DataModels/Group'
 
-function buildReport(data, groups) {
+function buildReport(data, outgoingGroups, incomingGroups) {
+  const outgoingReportData = _sortReportData(
+    addGroupItems(data, outgoingGroups, (item) => extractPaidOut(item))
+  )
+  const incomingReportData = _sortReportData(
+    addGroupItems(data, incomingGroups, (item) => extractPaidIn(item))
+  )
+  console.log(incomingReportData)
+
+  return {
+    Income: incomingReportData,
+    Outgoing: outgoingReportData,
+  }
+}
+
+function addGroupItems(data, groups, priceFunction) {
   let reportData = {
-    Spends: 0,
+    Amount: 0,
   }
 
   for (const csvEntry of data) {
-    const csvOutgoings = extractOutgoings(csvEntry)
-    reportData.Spends += csvOutgoings
+    const csvAmount = priceFunction(csvEntry)
 
-    const groupResult = _runThroughGroups(reportData, groups, csvEntry)
+    if (csvAmount === 0) {
+      continue
+    }
+    reportData.Amount += csvAmount
+
+    const groupResult = _runThroughGroups(reportData, groups, csvEntry, priceFunction)
     reportData = groupResult[1]
 
     if(!groupResult[0]) {
       reportData = _setupReportEntry(reportData, DEFAULT_GROUP)
-      reportData[DEFAULT_GROUP.name].Entries.push(_buildEntry(csvEntry))
-      reportData[DEFAULT_GROUP.name].Spends += csvOutgoings
+      reportData[DEFAULT_GROUP.name].Entries.push(_buildEntry(csvEntry, priceFunction))
+      reportData[DEFAULT_GROUP.name].Amount += csvAmount
     }
   }
 
+  return reportData
+}
+
+function _sortReportData(reportData) {
   for(const group in reportData) {
-    if (group === 'Spends') continue
+    if (group === 'Amount') continue
 
     if(reportData[group].Entries) {
       reportData[group].Entries.sort((a,b) => b.Price - a.Price)
     } else {
       for (const subGroup in reportData[group]) {
-        if (subGroup === 'Spends') continue
+        if (subGroup === 'Amount') continue
         if (reportData[group][subGroup].Entries) {
           reportData[group][subGroup].Entries.sort((a,b) => b.Price - a.Price)
         }
@@ -43,7 +67,7 @@ function buildReport(data, groups) {
   return reportData
 }
 
-function _runThroughGroups(reportData, groups, csvEntry) {
+function _runThroughGroups(reportData, groups, csvEntry, priceFunction) {
   for (const group of groups) {
     const containingGroup = group.containsEntry(csvEntry)
 
@@ -51,7 +75,7 @@ function _runThroughGroups(reportData, groups, csvEntry) {
       const subGroup = containingGroup[1]
 
       reportData = _setupReportEntry(reportData, group, subGroup)
-      reportData = _addEntry(reportData, group.name, subGroup.name, csvEntry)
+      reportData = _addEntry(reportData, group.name, subGroup.name, csvEntry, priceFunction)
 
       return [true, reportData]
     }
@@ -60,20 +84,20 @@ function _runThroughGroups(reportData, groups, csvEntry) {
   return [false, reportData]
 }
 
-function _buildEntry(csvEntry) {
+function _buildEntry(csvEntry, priceFunction) {
   return {
     Date: extractDate(csvEntry),
-    Price: extractOutgoings(csvEntry),
+    Price: priceFunction(csvEntry),
     Description: extractDescription(csvEntry),
   }
 }
 
-function _addEntry(currentData, groupName, subGroupName, csvEntry) {
-  const entry = _buildEntry(csvEntry)
+function _addEntry(currentData, groupName, subGroupName, csvEntry, priceFunction) {
+  const entry = _buildEntry(csvEntry, priceFunction)
 
   currentData[groupName][subGroupName].Entries.push(entry)
-  currentData[groupName][subGroupName].Spends += entry.Price
-  currentData[groupName].Spends += entry.Price
+  currentData[groupName][subGroupName].Amount += entry.Price
+  currentData[groupName].Amount += entry.Price
 
   return currentData
 }
@@ -86,16 +110,16 @@ function _setupReportEntry(currentData, group, subGroup) {
     // if there's no subgroup name, we're setting up the global misc group,
     // if there is, we're setting up a standard subgroup
     currentData[groupName] = subGroupName ? {
-      Spends: 0,
+      Amount: 0,
     } : {
-      Spends: 0,
+      Amount: 0,
       Entries: [],
     }
   }
   if (subGroupName && !currentData[groupName][subGroupName]) {
     currentData[groupName][subGroupName] = {
       Entries: [],
-      Spends: 0
+      Amount: 0
     }
   }
 
